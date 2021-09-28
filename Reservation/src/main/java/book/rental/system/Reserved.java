448376d6b628557ce04811cc59a3bea5ad7a27b5
Kooyoungjiin <33479996+Kooyoungjiin@users.noreply.github.com>
2021-09-28 22:49:27 +0900

    Add files via upload

---
+++ Reservation/src/main/java/book/rental/system/Reserved.java
@@ -0,0 +1,60 @@
+package book.rental.system;
+
+import java.util.Date;
+
+public class Reserved extends AbstractEvent {
+
+    private Long id;
+    private Long bookId;
+    private Long customerId;
+    private Date startDate;
+    private Date endDate;
+    private String reservationStatus;
+
+    public Reserved(){
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    public String getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(String reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
+}
